02c7660d4aa6729e2b632e430362e813545c71c0
lengsukq <lengsukq@foxmail.com>
2024-10-22 11:04:23 +0800

    自定义环境变量

--- routes/command_executor.py
+++ routes/command_executor.py
@@ -12,7 +12,7 @@ lock = threading.Lock()
 # 存储压缩文件路径
 compressed_file_path = None
 
-def stream_output(process):
+def stream_output(process, env_cus_path):
     global compressed_file_path
     try:
         while True:
@@ -37,6 +37,12 @@ def stream_output(process):
             yield f"\n下载链接: /download/{zip_filename}\n"
         else:
             yield "\n未找到 dist 文件夹\n"
+
+        # 删除 .env.cus 文件
+        if os.path.exists(env_cus_path):
+            os.remove(env_cus_path)
+            yield "\n.env.cus 文件已删除\n"
+
     except Exception as e:
         yield f"\n错误: {str(e)}\n"
 
@@ -53,11 +59,27 @@ def command_executor_route(app):
             # 获取命令行参数
             command = request.json.get('command')
             selected_branch = request.json.get('selectedBranch')
+            api_base_url = request.json.get('apiBaseUrl')
+            web_base_url = request.json.get('webBaseUrl')
+            cas_base_url = request.json.get('casBaseUrl')
+            online_base_url = request.json.get('onlineBaseUrl')
+
+            # 如果没有传命令，则默认执行 vue-cli-service serve --mode cus
             if not command:
-                return make_response("未提供命令", 200)
-            # 检查命令是否以 'yarn' 开头
-            if not command.startswith('yarn'):
-                return make_response({"命令必须是 'yarn' 开头"}, 200)
+                command = 'vue-cli-service serve --mode cus'
+
+                # 检查必传参数
+                if not selected_branch or not api_base_url or not web_base_url or not cas_base_url or not online_base_url:
+                    return make_response("未提供必填项", 200)
+            else:
+                # 检查必传参数
+                if not selected_branch:
+                    return make_response("未提供必填项", 200)
+
+            # 检查命令是否以 'yarn' 或 'vue-cli-service' 开头
+            if not command.startswith('yarn') and not command.startswith('vue-cli-service'):
+                return make_response({"命令必须是 'yarn' 或 'vue-cli-service' 开头"}, 200)
+
             # 获取环境变量 AUTO_BUILD_SHELL_PATH 的值
             auto_build_shell_path = os.getenv('AUTO_BUILD_SHELL_PATH')
             if not auto_build_shell_path:
@@ -69,10 +91,21 @@ def command_executor_route(app):
             except FileNotFoundError:
                 return make_response(f"目录 {auto_build_shell_path} 不存在", 200)
 
+            # 生成 .env.cus 文件
+            env_cus_path = os.path.join(auto_build_shell_path, '.env.cus')
+            with open(env_cus_path, 'w') as f:
+                f.write(f"NODE_ENV=cus\n")
+                f.write(f"VUE_APP_API_BASE_URL={api_base_url}\n")
+                f.write(f"VUE_APP_WEB_BASE_URL={web_base_url}\n")
+                f.write(f"VUE_APP_CAS_BASE_URL={cas_base_url}\n")
+                f.write(f"VUE_APP_ONLINE_BASE_URL={online_base_url}\n")
+
             # 如果提供了 selectedBranch 参数，切换分支并执行相关操作
             if selected_branch:
                 # 拼接命令
                 full_command = f"git fetch origin && git checkout -f {selected_branch} && git reset --hard origin/{selected_branch} && yarn install --no-lockfile && {command}"
+            else:
+                full_command = f"yarn install --no-lockfile && {command}"
 
             # 启动命令行任务
             try:
@@ -92,7 +125,7 @@ def command_executor_route(app):
         timer.start()
 
         # 实时输出命令行结果
-        response = Response(stream_output(current_process), mimetype='text/plain')
+        response = Response(stream_output(current_process, env_cus_path), mimetype='text/plain')
 
         # 停止计时器
         timer.cancel()
